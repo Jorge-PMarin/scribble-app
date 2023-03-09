b02b8fdd33a46b4fc4a24c69b3d3eea7a777526d
jorge luis puerta marin <jorgeluis.sk8@hotmail.com>
2023-03-09 09:02:26 -0500

    Write component finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import NavBar from "./components/NavBar";
-import Post from "./components/Post";
-import HomePage from "./Pages/HomePage";
-import PostPage from "./Pages/PostPage";
+import NavBar from './components/NavBar';
+import Post from './components/Post';
+import HomePage from './Pages/HomePage';
+import PostPage from './Pages/PostPage';
+import Write from './components/write';
+import UserBar from './components/UserBar';
 
 function App() {
-
   return (
     <>
-      <NavBar />
-      <PostPage />
+      <UserBar />
+      <Write />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Write.jsx
@@ -0,0 +1,34 @@
+import TextareaAutosize from 'react-textarea-autosize';
+import imgURL from '../../public/notebookOnTable.jpg';
+
+export default function Write() {
+  return (
+    <main className="write">
+      <form className="write__form">
+        <div className="write__form__group">
+          <label htmlFor="write-img" className="write__form__plus">
+            <i className="fa-solid fa-plus"></i>
+          </label>
+          <input type="file" id="write-img" className="write__form__file" />
+          <input
+            type="text"
+            className="write__form__title"
+            placeholder="Title"
+          />
+        </div>
+        <div className="write__form__group">
+          <img src={imgURL} alt="" className="write__form__img" />
+        </div>
+
+        <div className="write__form__group">
+          <TextareaAutosize
+            className="write__form__body"
+            name="post-body"
+            placeholder="Tell your story..."
+          />
+        </div>
+        <button className="write__form__btn">Publish</button>
+      </form>
+    </main>
+  );
+}
